993357d346e19db9839afa9432f0f072993682bd
tommaso santoro <tommaso.santoro@studenti.unicam.it>
2026-06-17 16:42:58 +0200

    Creazione classe Nemico con attributi e metodi getter e setter

---
+++ app/src/main/java/it/unicam/cs/mpgc/rpg129078/Nemico.java
@@ -0,0 +1,76 @@
+package it.unicam.cs.mpgc.rpg129078;
+
+public class Nemico {
+
+    private String nome;
+
+    private int vitaMassima;
+    private int vitaCorrente;
+
+    private int attacco;
+
+    private TipoNemico tipo;
+
+    public Nemico(String nome, int vitaMassima, int attacco, TipoNemico tipo) {
+        this.nome = nome;
+
+        this.vitaMassima = Math.max(0, vitaMassima);
+        this.vitaCorrente = this.vitaMassima;
+
+        this.attacco = Math.max(0, attacco);
+
+        this.tipo = tipo;
+    }
+
+   
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getVitaMassima() {
+        return vitaMassima;
+    }
+
+    public int getVitaCorrente() {
+        return vitaCorrente;
+    }
+
+    public int getAttacco() {
+        return attacco;
+    }
+
+    public TipoNemico getTipo() {
+        return tipo;
+    }
+
+    // ===== SETTER =====
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setVitaMassima(int vitaMassima) {
+        this.vitaMassima = Math.max(0, vitaMassima);
+    }
+
+    public void setVitaCorrente(int vitaCorrente) {
+        if (vitaCorrente < 0) {
+            this.vitaCorrente = 0;
+        } else if (vitaCorrente > vitaMassima) {
+            this.vitaCorrente = vitaMassima;
+        } else {
+            this.vitaCorrente = vitaCorrente;
+        }
+    }
+
+    public void setAttacco(int attacco) {
+        this.attacco = Math.max(0, attacco);
+    }
+
+    public void setTipo(TipoNemico tipo) {
+        this.tipo = tipo;
+    }
+
+
+}
---
+++ app/src/main/java/it/unicam/cs/mpgc/rpg129078/TipoNemico.java
@@ -0,0 +1,6 @@
+package it.unicam.cs.mpgc.rpg129078;
+
+public enum TipoNemico {
+    NORMALE,
+    BOSS
+}
